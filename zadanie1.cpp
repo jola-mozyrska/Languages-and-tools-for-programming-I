2a1489424bad776698c260ce39b8983bc7548655
Jolanta Mozyrska <jola@qed.ai>
2019-10-15 15:41:18 +0200

    Passing sample tests.

--- zadanie1.cpp
+++ zadanie1.cpp
@@ -9,8 +9,6 @@
 
 using namespace std;
 
-// TODO:test
-
 const int mx_time = 950;
 const long long INF = numeric_limits<double>::infinity();
 
@@ -41,8 +39,7 @@ bool add_tram(const long long tram_number, vector<int> &schedule_time,
               vector<string> &tram_stops,
               unordered_map<long long, unordered_map<string, int>> &schedule_for_trams) {
 
-    // ull?
-    for (unsigned long long i = 0; i < tram_stops.size(); ++i) {
+    for (auto i = 0; i < tram_stops.size(); ++i) {
         auto stop_name = tram_stops[i];
         auto stop_time = schedule_time[i];
 
@@ -56,7 +53,7 @@ bool add_tram(const long long tram_number, vector<int> &schedule_time,
     return true;
 }
 
-//  adds new vailable ticket and updates optimal cost for given time
+//  adds new avilable ticket and updates optimal cost for given time
 void add_ticket(string &name, const long long price, long long minutes,
                 vector<string> &ticket_name,
                 vector<long long> &ticket_price, vector<long long> &ticket_time,
@@ -189,8 +186,7 @@ void tests() {
 //    Dlugi Tani 3.70 60
 //    Niedobry 4.333 39 ignore
 //    ? Banacha 1 Winnicka
-//    ? Banacha 1 Winnicka 4 Banacha loop
-
+//    ? Banacha 1 Winnicka 4 Banacha
 
     vector<string> names = {"Banacha", "Pasteura", "Winnicka"};
     vector<int> hours = {6 * 60, 7 * 60, 8 * 60};
@@ -216,193 +212,193 @@ void tests() {
     vector<long long> nr = {(long long) 1};
     ask_for_tickets(names, nr, proposed_tickets, schedule_for_trams,
                     number_of_tickets);
-    names = {"Banacha", "Winnicka"};
-    nr = {(long long) 1};
+    names = {"Banacha", "Winnicka", "Banacha"};
+    nr = {(long long) 1, (long long) 4};
     ask_for_tickets(names, nr, proposed_tickets, schedule_for_trams,
                     number_of_tickets);
 
     cout << number_of_tickets << "\n";
 }
 
-//int main() {
-////    tests();
-//}
-
-const int lower_time_limit = 355;
-const int upper_time_limit = 1281;
-
-const int minutes_in_hour = 60;
-const int pennys_in_price_integral = 100;
-
-const regex assistant_regex("[0-9]+|[a-zA-Z_^]+");
-const regex question_regex("[?]\\s[a-zA-Z_^]+(?:\\s[0-9]+\\s[a-zA-Z_^]+)+");
-const regex course_regex("[0-9]+(?:\\s[1-9]?[0-9]:[0-9]{2}\\s[a-zA-Z_^]+)+");
-const regex ticket_regex(
-        "([a-zA-Z]+(?:\\s[a-zA-Z]+)*)\\s([0-9]+)[.]([0-9]{2})\\s([0-9]+)");
-
-void print_error(string line, int line_number) {
-    cerr << "Error in line " << line_number << ": " << line << "\n";
-}
-
-int number_of_whitespaces(string line) {
-    int counter = 0;
-
-    for (size_t i = 0; i < line.size(); i++) {
-        if (line[i] == ' ') {
-            counter++;
-        }
-    }
-
-    return counter;
-}
-
-int time_to_minutes(int hours, int minutes) {
-    return hours * minutes_in_hour + minutes;
-}
-
-long long price_to_pennys(long price_integral, int price_fractional) {
-    return price_integral * pennys_in_price_integral + price_fractional;
-}
-
-void question_about_ticket(string line, int line_number,
-                           vector<string> &stops,
-                           vector<long long> &trams_numbers) {
-    bool error = false;
-    int number_of_stops = 0;
-
-    if (regex_match(line, question_regex)) {
-        sregex_iterator i = sregex_iterator(line.begin(),
-                                            line.end(), assistant_regex);
-        number_of_stops = (number_of_whitespaces(line) + 1) / 2;
-        stops = vector<string>(number_of_stops);
-        trams_numbers = vector<long long>(number_of_stops - 1);
-        int j = 0;
-        stops.at(j) = (*i).str();
-        i++;
-        while ((i != sregex_iterator()) && !error) {
-            trams_numbers.at(j) = stoll((*i).str());
-            if (is_tram_number_correct(trams_numbers.at(j))) {
-                i++;
-                stops.at(j + 1) = (*i).str();
-                if (!check_if_tram_at_stop(trams_numbers.at(j), stops.at(j))
-                    || !check_if_tram_at_stop(trams_numbers.at(j),
-                                              stops.at(j + 1))) {
-                    error = true;
-                }
-                i++;
-                j++;
-            } else {
-                error = true;
-            }
-        }
-        if (!ask_for_tickets(stops, trams_numbers)) {
-            error = true;
-        }
-    } else {
-        error = true;
-    }
-
-    if (error) {
-        print_error(line, line_number);
-    }
-}
-
-void add_tram(string line, int line_number, long long &tram_number,
-              vector<string> &stops, vector<int> &times) {
-    bool error = false;
-    int number_of_stops = 0;
-
-    if (regex_match(line, course_regex)) {
-        sregex_iterator i = sregex_iterator(line.begin(),
-                                            line.end(), assistant_regex);
-        tram_number = stoll((*i).str());
-        i++;
-        if (is_tram_number_correct(tram_number)) {
-            number_of_stops = number_of_whitespaces(line) / 2;
-            stops = vector<string>(number_of_stops);
-            times = vector<int>(number_of_stops);
-            int j = 0, current_time = 0, previous_time = 0, hour = 0;
-            while ((i != sregex_iterator()) && !error) {
-                hour = stoi((*i).str());
-                i++;
-                current_time = time_to_minutes(hour, stoi((*i).str()));
-                if ((current_time >= lower_time_limit)
-                    && (current_time <= upper_time_limit)) {
-                    i++;
-                    times.at(j) = current_time;
-                    stops.at(j) = (*i).str();
-                    i++;
-                    j++;
-                    if (current_time <= previous_time) {
-                        error = true;
-                    }
-                    previous_time = current_time;
-                } else {
-                    error = true;
-                }
-            }
-        } else {
-            error = true;
-        }
-    } else {
-        error = true;
-    }
-
-    if (error) {
-        print_error(line, line_number);
-    }
-}
-
-void add_ticket(string line, int line_number,
-                string &ticket_name, long long &price, long long &validity) {
-    smatch result;
-
-    if (regex_match(line, result, ticket_regex)) {
-        smatch::iterator i = result.begin();
-        long price_integral;
-        i++;
-        ticket_name = *i;
-        i++;
-        price_integral = stol((*i).str());
-        i++;
-        price = price_to_pennys(price_integral, stoi((*i).str()));
-        i++;
-        validity = stoll(*i);
-    } else {
-        print_error(line, line_number);
-    }
-}
-
-void choose_function(string line, int line_number) {
-    char first_sign = line[0];
-    string ticket_name = "";
-    long long price = 0;
-    long long validity = 0;
-    long long tram_number = 0;
-    vector<string> stops;
-    vector<long long> trams_numbers;
-    vector<int> times;
-
-    if (first_sign == '?') {
-        question_about_ticket(line, line_number, stops, trams_numbers);
-    } else if ((first_sign >= '0') && (first_sign <= '9')) {
-        add_tram(line, line_number, tram_number, stops, times);
-    } else if (((first_sign >= 'A') && (first_sign <= 'Z'))
-               || ((first_sign >= 'a') && (first_sign <= 'z'))) {
-        add_ticket(line, line_number, ticket_name, price, validity);
-    } else {
-        print_error(line, line_number);
-    }
-}
-
 int main() {
-    string line;
-    int line_number = 0;
-
-    while (!getline(cin, line).eof()) {
-        line_number++;
-        if (!line.empty()) {
-            choose_function(line, line_number);
-        }
-    }
+    tests();
 }
+
+//const int lower_time_limit = 355;
+//const int upper_time_limit = 1281;
+//
+//const int minutes_in_hour = 60;
+//const int pennys_in_price_integral = 100;
+//
+//const regex assistant_regex("[0-9]+|[a-zA-Z_^]+");
+//const regex question_regex("[?]\\s[a-zA-Z_^]+(?:\\s[0-9]+\\s[a-zA-Z_^]+)+");
+//const regex course_regex("[0-9]+(?:\\s[1-9]?[0-9]:[0-9]{2}\\s[a-zA-Z_^]+)+");
+//const regex ticket_regex(
+//        "([a-zA-Z]+(?:\\s[a-zA-Z]+)*)\\s([0-9]+)[.]([0-9]{2})\\s([0-9]+)");
+//
+//void print_error(string line, int line_number) {
+//    cerr << "Error in line " << line_number << ": " << line << "\n";
+//}
+//
+//int number_of_whitespaces(string line) {
+//    int counter = 0;
+//
+//    for (size_t i = 0; i < line.size(); i++) {
+//        if (line[i] == ' ') {
+//            counter++;
+//        }
+//    }
+//
+//    return counter;
+//}
+//
+//int time_to_minutes(int hours, int minutes) {
+//    return hours * minutes_in_hour + minutes;
+//}
+//
+//long long price_to_pennys(long price_integral, int price_fractional) {
+//    return price_integral * pennys_in_price_integral + price_fractional;
+//}
+//
+//void question_about_ticket(string line, int line_number,
+//                           vector<string> &stops,
+//                           vector<long long> &trams_numbers) {
+//    bool error = false;
+//    int number_of_stops = 0;
+//
+//    if (regex_match(line, question_regex)) {
+//        sregex_iterator i = sregex_iterator(line.begin(),
+//                                            line.end(), assistant_regex);
+//        number_of_stops = (number_of_whitespaces(line) + 1) / 2;
+//        stops = vector<string>(number_of_stops);
+//        trams_numbers = vector<long long>(number_of_stops - 1);
+//        int j = 0;
+//        stops.at(j) = (*i).str();
+//        i++;
+//        while ((i != sregex_iterator()) && !error) {
+//            trams_numbers.at(j) = stoll((*i).str());
+//            if (is_tram_number_correct(trams_numbers.at(j))) {
+//                i++;
+//                stops.at(j + 1) = (*i).str();
+//                if (!check_if_tram_at_stop(trams_numbers.at(j), stops.at(j))
+//                    || !check_if_tram_at_stop(trams_numbers.at(j),
+//                                              stops.at(j + 1))) {
+//                    error = true;
+//                }
+//                i++;
+//                j++;
+//            } else {
+//                error = true;
+//            }
+//        }
+//        if (!ask_for_tickets(stops, trams_numbers)) {
+//            error = true;
+//        }
+//    } else {
+//        error = true;
+//    }
+//
+//    if (error) {
+//        print_error(line, line_number);
+//    }
+//}
+//
+//void add_tram(string line, int line_number, long long &tram_number,
+//              vector<string> &stops, vector<int> &times) {
+//    bool error = false;
+//    int number_of_stops = 0;
+//
+//    if (regex_match(line, course_regex)) {
+//        sregex_iterator i = sregex_iterator(line.begin(),
+//                                            line.end(), assistant_regex);
+//        tram_number = stoll((*i).str());
+//        i++;
+//        if (is_tram_number_correct(tram_number)) {
+//            number_of_stops = number_of_whitespaces(line) / 2;
+//            stops = vector<string>(number_of_stops);
+//            times = vector<int>(number_of_stops);
+//            int j = 0, current_time = 0, previous_time = 0, hour = 0;
+//            while ((i != sregex_iterator()) && !error) {
+//                hour = stoi((*i).str());
+//                i++;
+//                current_time = time_to_minutes(hour, stoi((*i).str()));
+//                if ((current_time >= lower_time_limit)
+//                    && (current_time <= upper_time_limit)) {
+//                    i++;
+//                    times.at(j) = current_time;
+//                    stops.at(j) = (*i).str();
+//                    i++;
+//                    j++;
+//                    if (current_time <= previous_time) {
+//                        error = true;
+//                    }
+//                    previous_time = current_time;
+//                } else {
+//                    error = true;
+//                }
+//            }
+//        } else {
+//            error = true;
+//        }
+//    } else {
+//        error = true;
+//    }
+//
+//    if (error) {
+//        print_error(line, line_number);
+//    }
+//}
+//
+//void add_ticket(string line, int line_number,
+//                string &ticket_name, long long &price, long long &validity) {
+//    smatch result;
+//
+//    if (regex_match(line, result, ticket_regex)) {
+//        smatch::iterator i = result.begin();
+//        long price_integral;
+//        i++;
+//        ticket_name = *i;
+//        i++;
+//        price_integral = stol((*i).str());
+//        i++;
+//        price = price_to_pennys(price_integral, stoi((*i).str()));
+//        i++;
+//        validity = stoll(*i);
+//    } else {
+//        print_error(line, line_number);
+//    }
+//}
+//
+//void choose_function(string line, int line_number) {
+//    char first_sign = line[0];
+//    string ticket_name = "";
+//    long long price = 0;
+//    long long validity = 0;
+//    long long tram_number = 0;
+//    vector<string> stops;
+//    vector<long long> trams_numbers;
+//    vector<int> times;
+//
+//    if (first_sign == '?') {
+//        question_about_ticket(line, line_number, stops, trams_numbers);
+//    } else if ((first_sign >= '0') && (first_sign <= '9')) {
+//        add_tram(line, line_number, tram_number, stops, times);
+//    } else if (((first_sign >= 'A') && (first_sign <= 'Z'))
+//               || ((first_sign >= 'a') && (first_sign <= 'z'))) {
+//        add_ticket(line, line_number, ticket_name, price, validity);
+//    } else {
+//        print_error(line, line_number);
+//    }
+//}
+//
+//int main() {
+//    string line;
+//    int line_number = 0;
+//
+//    while (!getline(cin, line).eof()) {
+//        line_number++;
+//        if (!line.empty()) {
+//            choose_function(line, line_number);
+//        }
+//    }
+//}
