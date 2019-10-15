cdb50002575dff9e5f096a4f022f779371554180
Jolanta Mozyrska <jola@qed.ai>
2019-10-15 09:05:56 +0200

    Corrected knapsack logic.

--- zadanie1.cpp
+++ zadanie1.cpp
@@ -18,8 +18,8 @@ using namespace std;
 //vector<double> ticket_price;
 //vector<long long> ticket_time;
 //
-//vector<long long> cost(mx_time, INF);
-//vector<vector<long long>> proposed_tickets(mx_time + 4);
+//vector<long long> cost(mx_time + 2, INF);
+//vector<vector<long long>> proposed_tickets(mx_time + 2);
 
 const int mx_time = 950;
 const long long INF = numeric_limits<double>::infinity();
@@ -90,6 +90,17 @@ void add_ticket(string &name, const long long price, long long minutes,
 
                 proposed_tickets[i] = proposed_tickets[i - minutes];
                 proposed_tickets[i].push_back(name);
+            }
+
+            if(i < mx_time) {
+                if(cost[i] > cost[i+1]) {
+                    cost[i] = cost[i+1];
+                    proposed_tickets[i] = proposed_tickets[i+1];
+                }
+                else if(cost[i] == cost[i+1] && proposed_tickets[i].size() > proposed_tickets[i+1].size()) {
+                    cost[i] = cost[i+1];
+                    proposed_tickets[i] = proposed_tickets[i+1];
+                }
             }
 
         }
