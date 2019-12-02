d1750f45d1db9a0b3020cb6c64e5b836db5c2eab
nataliakuc <50085368+nataliakuc@users.noreply.github.com>
2019-12-02 17:24:56 +0100

    Update fibin.h

--- zadanie4/fibin.h
+++ zadanie4/fibin.h
@@ -1,99 +1,75 @@
 #ifndef PROJECT4_FIBIN_H
 #define PROJECT4_FIBIN_H
 
+template<typename T1>
+class Fibin {
+    // static List
 
-constexpr int Fibo(uint I) {
-    return I;// TODO i-ta liczba Fibonacciego
+    template<typename T2>
+
+
+    template <typename T>
+    constexpr int eval() {}
+
+    template <Lit<typename T>>
+    constexpr int eval() {
+
+    }
+
+    struct Var {
+        const char *name;
+        Var(const char *N): name(N) {}
+    };
+
+    template <Ref<typename T>>
+    constexpr int eval() {}
+
+    template <Let<Var V, typename Val, typename E>>
+    constexpr int eval() {
+        return Let<V::name, Val, E>.fun();
+        //Let<V::name, Val, E, Environment>.fun();
+    }
+};
+
+template <const char* N, typename Val, typename E>//, List Env>
+struct Let {
+    constexpr int fun() {return 1;}
+
+    // Env.append(
+};
+
+/*
+class VarClass {
+    const char* name;
+    auto val;
+
+    VarClass(const char* name): name(name) {}
+
+    template<typename T>
+    void set(T t) const{
+        val = t;
+    }
+}*/
+
+
+template <const char *N>
+struct Ref {
+    template <typename T>
+    struct {
+        static T val;
+    };
+/*
+    constexpr static int get() const {return val;}
+
+    constexpr static void set(T v) {val = v;}
+*/
+};
+
+template <int I, typename T t, typename E>
+class Let {
+    constexpr static E fun() {
+        E<Ref<I, T>::val = t > result;
+    }
 }
 
-template<>
-struct True {
-    static const int val = 1;
-};
-
-template<>
-struct False {
-    static const int val = 0;
-};
-
-template <uint I> //TODO może inny typ, żeby wszystkie uint, int64 itd wchodziły
-struct Fib {
-    static const int val = Fibo(I);
-};
-
-template <typename T>
-struct Lit {
-    static const int val = T::val;
-};
-
-
-template<typename T>
-class Fibin {
-    template<typename T2>
-    constexpr T eval() {T2();}
-};
-
-
-//
-//Literały Lit
-//Literałami liczbowymi mogą być tylko liczby Fibonacciego, np. Fib<0> = 0, Fib<1> = 1 itd.
-//Parametry Fib muszą być nieujemne.
-//Literałami logicznymi mogą być tylko dwie wartości logiczne: True, False.
-//Przykłady poprawnych literałów: Lit<Fib<0>>, Lit<True>.
-//
-//* Zmienne Var
-//Var(const char*) - identyfikator zmiennej; identyfikatory tworzone są na podstawie
-//        ciągów znaków o długości od 1 do 6 włącznie, zawierających małe i wielkie litery
-//        alfabetu angielskiego (a-zA-Z) oraz cyfry (0-9); małe i wielkie litery nie są
-//rozróżniane.
-//Przykłady poprawnych zmiennych: Var("A"), Var("01234"), Var("Cdefg").
-//
-//* Operacje arytmetyczne Sum, Inc1, Inc10
-//        Sum<...> - operacja dodawania wielu liczb, wymagane są co najmniej dwa argumenty.
-//Inc1<Arg> - specjalizacja dodawania, która zwiększa wartość Arg o Fib<1>.
-//Inc10<Arg> - specjalizacja dodawania, która zwiększa wartość Arg o Fib<10>.
-//Przykłady poprawnych operacji: Sum<Lit<Fib<0>>, Lit<Fib<1>>, Lit<Fib<3>>>,
-//Inc1<Lit<Fib<0>>>.
-//
-//* Porównanie Eq
-//Eq<Left, Right> - porównuje wartość Left z Right; zwraca True, gdy są równe,
-//        a False w przeciwnym przypadku.
-//Przykład poprawnego porównania: Eq<Lit<Fib<0>>, Lit<Fib<1>>>.
-//
-//* Odwołanie do zmiennej Ref
-//Ref<Var> - zwraca wartość zmiennej identyfikowanej przez Var.
-//Przykład poprawnego odwołania do zmiennej: Ref<Var("A")>.
-//
-//* Wyrażenie Let
-//Let<Var, Value, Expression> - do zmiennej Var przypisuje wartość Value
-//        i oblicza wartość Expression.
-//Przykład poprawnego wyrażenia: Let<Var("A"), Lit<Fib<1>>, Ref<Var("A")>>.
-//
-//* Wyrażenie If
-//If<Condition, Then, Else> - jeśli Condition jest True, to oblicza wartość Then,
-//a w przeciwnym przypadku oblicza wartość Else.
-//Przykład poprawnego wyrażenia: If<Lit<True>, Lit<Fib<1>>, Lit<Fib<0>>>.
-//
-//* Wyrażenie Lambda
-//Lambda<Var, Body> - reprezentuje anonimową funkcję z jednym parametrem Var
-//        oraz ciałem Body.
-//Przykład poprawnego wyrażenia: Lambda<Var("x"), Ref<Var("x")>>.
-//
-//* Wywołanie funkcji
-//Invoke<Fun, Param> - oblicza wartość funkcji Fun dla parametru Param.
-//Przykład poprawnego wyrażenia: Invoke<Lambda<Var("x"), Ref<Var("x")>>, Lit<Fib<0>>>.
-//
-//Należy dostarczyć klasę szablonową Fibin:
-//
-//* Fibin<ValueType> - ValueType jest typem wartości używanym do obliczania wartości
-//wyrażeń.
-//
-//Klasa Fibin udostępnia publiczną metodę szablonową:
-//
-//* ValueType eval<Expr> - metoda dostępna tylko wtedy, gdy ValueType jest typem liczbowym.
-//Oblicza w trakcie kompilacji wartość wyrażenia Expr i zwraca tę wartość.
-//
-//* void eval<Expr> - metoda dostępna tylko wtedy, gdy ValueType NIE jest typem liczbowym.
-//Wyświetla informację, że Fibin nie wspiera danego typu.
-//
 #endif //PROJECT4_FIBIN_H
