62b0ed3c1ac67c3d5076b9a8545908f488fd2f63
nataliakuc <50085368+nataliakuc@users.noreply.github.com>
2019-12-03 19:33:44 +0100

    Trochę testów

--- zadanie4/main.cpp
+++ zadanie4/main.cpp
@@ -5,6 +5,27 @@
 int main() {  
     cout << Fibin<int64_t>::eval<Lit<Fib<1>>>();
     
+    static_assert(Fibin<uint64_t>::eval<Lit<Fib<12>>>() == 144);
+    static_assert(Fibin<uint16_t>::eval<Lit<Fib<25>>>() == 9489);
+    static_assert(Fibin<bool>::eval<Lit<True>>);
+
+    static_assert(Fibin<uint8_t>::eval<Lit<Fib<12>>>() == 144);
+
+
+    static_assert(Fibin<uint32_t>::eval<Inc1<Lit<Fib<3>>>>() == 3);
+    static_assert(Fibin<uint32_t>::eval<Inc10<Lit<Fib<3>>>>() == 57);
+    static_assert(Fibin<uint32_t>::eval<Sum<Lit<Fib<0>>, Lit<Fib<1>>, Inc10<Lit<Fib<3>>>>>() == 58);
+
+    static_assert(Fibin<uint32_t>::eval<Ref<Var("a")>>() == 'a');
+
+    static_assert(Fibin<uint64_t>::eval<Eq<Lit<Fib<0>>, Lit<Fib<1>>>>() == 0);
+    static_assert(Fibin<uint64_t>::eval<Eq<Lit<Fib<0>>, Lit<True>>>() == 0);
+    static_assert(Fibin<uint64_t>::eval<Eq<Lit<Fib<0>>, Lit<False>>>() == 0);
+    static_assert(Fibin<uint64_t>::eval<Eq<Lit<Fib<1>>, Lit<Fib<2>>>>() == 1);
+    static_assert(Fibin<uint64_t>::eval<Eq<Lit<Fib<15>>, Lit<Fib<2>>>>() == 0);
+    static_assert(Fibin<uint64_t>::eval<Eq<If<Lit<True>, Lit<Fib<1>>, Lit<Fib<0>>>, Lit<Fib<1>>>>);
+
+    
     /*
     // Testing: lambda(x) {x + (Fib(1) + Fib(10)) + Fib(2)}(Fib(3))
     // Fib(0) = 0, Fib(1) = 1, Fib(2) = 1, Fib(3) = 2, Fib(10) = 55
