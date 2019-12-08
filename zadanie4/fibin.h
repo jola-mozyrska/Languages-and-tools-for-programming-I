5992027d4bd28fd0f5a20eecabb275aa34ad42d3
Jolanta Mozyrska <jola@qed.ai>
2019-12-08 21:14:01 +0100

    add namespace

--- zadanie4/fibin.h
+++ zadanie4/fibin.h
@@ -7,11 +7,6 @@ namespace details {
 
     // Struktury możliwych przyjmowanych wartości
 
-    template<typename T, T i>
-    struct Value {
-        constexpr static T val = i;
-    };
-
     template<unsigned I, typename ValueType>
     struct _Fib {
         static constexpr ValueType val = static_cast<ValueType>(_Fib<I - 1, ValueType>::val +
@@ -28,33 +23,10 @@ namespace details {
         static constexpr ValueType val = static_cast<ValueType>(1);
     };
 
-    template<unsigned I>
-    struct Fib {
-        template<typename ValueType>
-        using eval = Value<ValueType, _Fib<I, ValueType>::val>;
-    };
-
     template<bool B>
     struct Bool {
         constexpr static bool val = B;
     };
-
-    struct True {
-        template<typename ValueType>
-        using eval = Bool<true>;
-    };
-
-    struct False {
-        template<typename ValueType>
-        using eval = Bool<false>;
-    };
-
-    template<typename V>
-    struct Lit {
-        template<typename ValueType, typename List>
-        using eval = typename V::template eval<ValueType>;
-    };
-
 
     // Zmienne, do których istnieje dostęp pamiętane są w liście,
     // tworzy ona środowisko, w którym wykonują się funkcje.
@@ -77,69 +49,6 @@ namespace details {
         using result =  Value;
     };
 
-    // Funkcja hashująca dany ciąg znaków
-    constexpr uint64_t Var(const char *N) {
-        size_t i = 0;
-        uint64_t hash = 0;
-        uint64_t temp = 0;
-        const uint64_t BASE = 100;
-        while (N[i] != '\0') {
-            hash *= BASE;
-            temp = static_cast<int64_t>(('A' <= N[i] && N[i] <= 'Z') ? N[i] :
-                                        (('a' <= N[i] && N[i] <= 'z') ? N[i] - 'a' + 'A' :
-                                         (('0' <= N[i] && N[i] <= '9') ? N[i] : 0)));
-            if (temp == 0 || i >= 6)
-                return 0;
-            hash += static_cast<uint64_t>(temp);
-            ++i;
-        }
-
-        return hash;
-    }
-
-    template<uint64_t I>
-    struct Ref {
-        template<typename ValueType, typename List>
-        using eval = typename Find<I, List>::result;
-    };
-
-    template<uint64_t var, typename Val, typename Expr>
-    struct Let {
-        static_assert(var > 0);
-
-        template<typename ValueType, typename List>
-        using eval = typename Expr::template eval<ValueType, ListEnvironment<var, typename Val::template eval<ValueType, List>, List>>;
-    };
-
-    // Operacje arytmetyczne
-    template<typename T>
-    struct Inc1 {
-        template<typename ValueType, typename List>
-        using eval = Value<ValueType, static_cast<ValueType>(T::template eval<ValueType, List>::val +
-                                                             Fib<1>::template eval<ValueType>::val)>;
-    };
-
-    template<typename T>
-    struct Inc10 {
-        template<typename ValueType, typename List>
-        using eval = Value<ValueType, static_cast<ValueType>(T::template eval<ValueType, List>::val +
-                                                             Fib<10>::template eval<ValueType>::val)>;
-    };
-
-    template<typename T1, typename T2, typename... T3>
-    struct Sum {
-        template<typename ValueType, typename List>
-        using eval = Value<ValueType, static_cast<ValueType>(T1::template eval<ValueType, List>::val +
-                                                             Sum<T2, T3...>::template eval<ValueType, List>::val)>;
-    };
-
-    template<typename T1, typename T2>
-    struct Sum<T1, T2> {
-        template<typename ValueType, typename List>
-        using eval = Value<ValueType, static_cast<ValueType>(T1::template eval<ValueType, List>::val +
-                                                             T2::template eval<ValueType, List>::val)>;
-    };
-
 /**************************************************************************/
 /** EQ **/
 /**************************************************************************/
@@ -156,13 +65,6 @@ namespace details {
         using eval = Bool<true>;
     };
 
-    template<typename Left, typename Right>
-    struct Eq {
-        template<typename ValueType, typename List>
-        using eval = typename _Eq<typename Left::template eval<ValueType, List>,
-                typename Right::template eval<ValueType, List>>::template eval<ValueType, List>;
-    };
-
     template<bool B, typename Then, typename Else>
     struct _If {
         template<typename ValueType, typename List>
@@ -175,36 +77,132 @@ namespace details {
         using eval = typename Then::template eval<ValueType, List>;
     };
 
-    template<typename Condition, typename Then, typename Else>
-    struct If {
-        template<typename ValueType, typename List>
-        using eval = typename _If<Condition::template eval<ValueType, List>::val, Then, Else>::template eval<ValueType, List>;
-    };
-
     template<uint64_t I, typename Body, typename List>
     struct _Lambda {
     };
-
-    template<uint64_t I, typename Body>
-    struct Lambda {
-        template<typename ValueType, typename List>
-        using eval = _Lambda<I, Body, List>;
-    };
-
-    template<typename Fun, typename Param>
-    struct Invoke {
-        template<typename ValueType, typename List>
-        using eval = typename Invoke<typename Fun::template eval<ValueType, List>, Param>::template eval<ValueType, List>;
-    };
-
-    template<uint64_t I, typename Body, typename L, typename Param>
-    struct Invoke<_Lambda<I, Body, L>, Param> {
-        static_assert(I > 0);
-        template<typename ValueType, typename List>
-        using eval = typename Body::template eval<ValueType, ListEnvironment<I, typename Param::template eval<ValueType, List>, L>>;
-    };
 }
 
+struct True {
+    template<typename ValueType>
+    using eval = details::Bool<true>;
+};
+
+struct False {
+    template<typename ValueType>
+    using eval = details::Bool<false>;
+};
+
+template<typename T, T i>
+struct Value {
+    constexpr static T val = i;
+};
+
+template<unsigned I>
+struct Fib {
+    template<typename ValueType>
+    using eval = Value<ValueType, details::_Fib<I, ValueType>::val>;
+};
+
+template<typename V>
+struct Lit {
+    template<typename ValueType, typename List>
+    using eval = typename V::template eval<ValueType>;
+};
+
+// Funkcja hashująca dany ciąg znaków
+constexpr uint64_t Var(const char *N) {
+    size_t i = 0;
+    uint64_t hash = 0;
+    uint64_t temp = 0;
+    const uint64_t BASE = 100;
+    while (N[i] != '\0') {
+        hash *= BASE;
+        temp = static_cast<int64_t>(('A' <= N[i] && N[i] <= 'Z') ? N[i] :
+                                    (('a' <= N[i] && N[i] <= 'z') ? N[i] - 'a' + 'A' :
+                                     (('0' <= N[i] && N[i] <= '9') ? N[i] : 0)));
+        if (temp == 0 || i >= 6)
+            return 0;
+        hash += static_cast<uint64_t>(temp);
+        ++i;
+    }
+
+    return hash;
+}
+
+template<uint64_t I>
+struct Ref {
+    template<typename ValueType, typename List>
+    using eval = typename details::Find<I, List>::result;
+};
+
+template<uint64_t var, typename Val, typename Expr>
+struct Let {
+    static_assert(var > 0);
+
+    template<typename ValueType, typename List>
+    using eval = typename Expr::template eval<ValueType, details::ListEnvironment<var, typename Val::template eval<ValueType, List>, List>>;
+};
+
+// Operacje arytmetyczne
+template<typename T>
+struct Inc1 {
+    template<typename ValueType, typename List>
+    using eval = Value<ValueType, static_cast<ValueType>(T::template eval<ValueType, List>::val +
+                                                         Fib<1>::template eval<ValueType>::val)>;
+};
+
+template<typename T>
+struct Inc10 {
+    template<typename ValueType, typename List>
+    using eval = Value<ValueType, static_cast<ValueType>(T::template eval<ValueType, List>::val +
+                                                         Fib<10>::template eval<ValueType>::val)>;
+};
+
+template<typename T1, typename T2, typename... T3>
+struct Sum {
+    template<typename ValueType, typename List>
+    using eval = Value<ValueType, static_cast<ValueType>(T1::template eval<ValueType, List>::val +
+                                                         Sum<T2, T3...>::template eval<ValueType, List>::val)>;
+};
+
+template<typename T1, typename T2>
+struct Sum<T1, T2> {
+    template<typename ValueType, typename List>
+    using eval = Value<ValueType, static_cast<ValueType>(T1::template eval<ValueType, List>::val +
+                                                         T2::template eval<ValueType, List>::val)>;
+};
+
+template<typename Left, typename Right>
+struct Eq {
+    template<typename ValueType, typename List>
+    using eval = typename details::_Eq<typename Left::template eval<ValueType, List>,
+            typename Right::template eval<ValueType, List>>::template eval<ValueType, List>;
+};
+
+template<typename Condition, typename Then, typename Else>
+struct If {
+    template<typename ValueType, typename List>
+    using eval = typename details::_If<Condition::template eval<ValueType, List>::val, Then, Else>::template eval<ValueType, List>;
+};
+
+template<typename Fun, typename Param>
+struct Invoke {
+    template<typename ValueType, typename List>
+    using eval = typename Invoke<typename Fun::template eval<ValueType, List>, Param>::template eval<ValueType, List>;
+};
+
+template<uint64_t I, typename Body, typename L, typename Param>
+struct Invoke<details::_Lambda<I, Body, L>, Param> {
+    static_assert(I > 0);
+    template<typename ValueType, typename List>
+    using eval = typename Body::template eval<ValueType, details::ListEnvironment<I, typename Param::template eval<ValueType, List>, L>>;
+};
+
+template<uint64_t I, typename Body>
+struct Lambda {
+    template<typename ValueType, typename List>
+    using eval = details::_Lambda<I, Body, List>;
+};
 
 template<typename T>
 struct Fibin {
