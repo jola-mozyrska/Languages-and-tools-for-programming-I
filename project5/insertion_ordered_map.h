52ab72ebfc2ec6463b250de7c635672e8a546c21
Jolanta Mozyrska <jola@qed.ai>
2019-12-19 22:24:18 +0100

    add constructors

--- project5/insertion_ordered_map.h
+++ project5/insertion_ordered_map.h
@@ -23,7 +23,7 @@ namespace {
         node<K>* node_in_list;
     };
 
-    class lookup_error{
+    class lookup_error : std::exception{
 
     };
 }
@@ -31,7 +31,8 @@ namespace {
 template <class K, class V, class Hash = std::hash<K>>
 class insertion_ordered_map {
 private:
-    std::unordered_map<K, value_in_map<K, V>, Hash> elements;
+    std::shared_ptr<std::unordered_map<K, value_in_map<K, V>, Hash>> elements_ptr;
+    std::shared_ptr<node<K>> first_node_ptr;
 
 public:
     insertion_ordered_map();
@@ -43,6 +44,7 @@ public:
 
     bool insert(K const &k, V const &v);
     void erase(K const &k);
+
 
     class iterator{
         iterator();
@@ -61,10 +63,55 @@ public:
 
 };
 
+// TODO: when to copy? when p1.use_count() > 1 and we want to change sth
+
 template <class K, class V, class Hash>
 insertion_ordered_map<K, V, Hash>::insertion_ordered_map() {
-
+    first_node_ptr = std::make_shared<node<K>>();
+    first_node_ptr = nullptr;
+    std::unordered_map<K, value_in_map<K, V>, Hash> m = {};
+    // TODO:&m
+    elements_ptr = std::make_shared<std::unordered_map<K, value_in_map<K, V>, Hash>>(&m);
 }
+
+template <class K, class V, class Hash>
+insertion_ordered_map<K, V, Hash>::insertion_ordered_map(insertion_ordered_map const &other) {
+    //  TODO: how to check if other is valid besides this, does it throw except?
+    if(other == this) return;
+
+    first_node_ptr = std::make_shared<node<K>>(other.first_node_ptr);
+    elements_ptr = std::make_shared<std::unordered_map<K, value_in_map<K, V>, Hash>>(other.elements_ptr);
+}
+
+template <class K, class V, class Hash>
+insertion_ordered_map<K, V, Hash>::insertion_ordered_map(insertion_ordered_map &&other) noexcept {
+    //  TODO: what to do when other == this?
+    if(other == this) return;
+
+    first_node_ptr = other.first_node_ptr;
+    elements_ptr = other.elements_ptr;
+
+    other.first_node_ptr = nullptr;
+    other.elements_ptr = nullptr;
+}
+
+//  TODO: why not & before other?
+// https://en.wikibooks.org/wiki/More_C%2B%2B_Idioms/Copy-and-swap
+template <class K, class V, class Hash>
+insertion_ordered_map<K, V, Hash> &
+insertion_ordered_map<K, V, Hash>::operator=(insertion_ordered_map<K, V, Hash> other) {
+    if(this != &other) {
+        //can i use cpy constructor for map?
+        // copy each node and join them?
+    }
+
+    return *this;
+}
+
+//  iterator
+
+
+
 
 
 
