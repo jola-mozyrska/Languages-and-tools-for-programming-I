91df6feb8a6d913c374d6c91e905fcd1781de920
Jolanta Mozyrska <jola@qed.ai>
2019-12-19 10:27:26 +0100

    add structures

--- project5/insertion_ordered_map.h
+++ project5/insertion_ordered_map.h
@@ -5,14 +5,35 @@
 
 template <class K, class V, class Hash = std::hash<K>>
 class insertion_ordered_map {
+private:
+    std::unordered_set<K, Hash> elements;
+
 public:
     insertion_ordered_map();
+    ~insertion_ordered_map() noexcept;
     insertion_ordered_map(insertion_ordered_map const &other);
-    insertion_ordered_map(insertion_ordered_map &&other);
+    insertion_ordered_map(insertion_ordered_map &&other) noexcept;
     insertion_ordered_map &operator=(insertion_ordered_map other);
+
 
     bool insert(K const &k, V const &v);
     void erase(K const &k);
+
+    class iterator{
+        iterator();
+        iterator(const iterator &other);
+        iterator& operator++();
+        bool operator==(iterator &other) const;
+        bool operator!=(iterator &other) const;
+        node<K, V>* operator->() const;
+        node<K, V>& operator*() const;
+    };
+
+    iterator begin();
+    iterator begin() const;
+    iterator end();
+    iterator end() const;
+
 };
 
 class lookup_error{
